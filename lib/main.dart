191139515920f91dc8cd7232b6de3a1bbbfe55e3
ClaitonTr <claiton07@gmail.com>
2021-04-14 22:45:56 -0300

    feat: enviroments

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/app/app_widget.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadEnv();
   runApp(AppWidget());
+}
+
+Future<void> loadEnv() async {
+  const isProduction = bool.fromEnvironment('dart.vm.product');
+  await DotEnv.load(fileName: isProduction ? '.env' : '.env_dev');
 }
